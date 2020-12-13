8c5a948b9c5e97c1ab66d98262303533a311572b
Marcin Szwed <49093128+szwedm@users.noreply.github.com>
2020-12-13 19:42:17 +0100

    Implement PDO & UserRepository

--- app/pdo/PDO.php
+++ app/pdo/PDO.php
@@ -1,3 +1,7 @@
 <?php
-
+try {
+	$db = new PDO('mysql:host=localhost;dbname=si-blog;port=3306', 'root');
+} catch (PDOException $e) {
+	echo 'Database connection error: '.$e->getMessage();
+}
 ?>
--- app/repositories/UserRepository.php
+++ app/repositories/UserRepository.php
@@ -1,3 +1,58 @@
 <?php
+spl_autoload_register(function ($class_name) {
+    include '..' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $class_name . '.php';
+});
 
+final class UserRepository {
+	
+	public function getUserById($userId) {
+		require '..' . DIRECTORY_SEPARATOR . 'pdo'. DIRECTORY_SEPARATOR . 'PDO.php';
+		$stmt = $db->prepare('SELECT * FROM User WHERE id = :userId');
+		$stmt->bindValue(':userId', $userId, PDO::PARAM_INT);
+		$stmt->execute();
+		$userInfo = $stmt->fetch();
+		$user = new User($userInfo['id'], $userInfo['name'], $userInfo['password'], $userInfo['role']);
+		$stmt->closeCursor();
+		$db = null;
+		
+		return $user;
+	}
+
+	public function getAllUsers() {
+		require '..' . DIRECTORY_SEPARATOR . 'pdo'. DIRECTORY_SEPARATOR . 'PDO.php';
+		$users = [];
+		$stmt = $db->query('SELECT * FROM User ORDER BY id');
+		while ($userInfo = $stmt->fetch()) {
+			$user = new User($userInfo['id'], $userInfo['name'], $userInfo['password'], $userInfo['role']);
+			array_push($users, $user);
+		}
+		$stmt->closeCursor();
+		$db = null;
+
+		return $users;
+	}
+
+	public function saveUserFromRequest($createUserRequest) {
+		require '..' . DIRECTORY_SEPARATOR . 'pdo'. DIRECTORY_SEPARATOR . 'PDO.php';
+		$stmt = $db->prepare('INSERT INTO User (name, password, role) VALUES (:name, :password, :role)');
+		$stmt->bindValue(':name', $createUserRequest->getName(), PDO::PARAM_STR);
+		$stmt->bindValue(':password', $createUserRequest->getPassword(), PDO::PARAM_STR);
+		$stmt->bindValue(':role', $createUserRequest->getRole(), PDO::PARAM_STR);
+		$stmt->execute();
+		$stmt->closeCursor();
+		$db = null;
+	}
+
+	public function updateUser($user) {
+		require '..' . DIRECTORY_SEPARATOR . 'pdo'. DIRECTORY_SEPARATOR . 'PDO.php';
+		$stmt = $db->prepare('UPDATE User SET name = :name, password = :password, role = :role WHERE id = :userId');
+		$stmt->bindValue(':name', $user->getName(), PDO::PARAM_STR);
+		$stmt->bindValue(':password', $user->getPassword(), PDO::PARAM_STR);
+		$stmt->bindValue(':role', $user->getRole(), PDO::PARAM_STR);
+		$stmt->bindValue(':userId', $user->getId(), PDO::PARAM_INT);
+		$stmt->execute();
+		$stmt->closeCursor();
+		$db = null;
+	}
+}
 ?>
