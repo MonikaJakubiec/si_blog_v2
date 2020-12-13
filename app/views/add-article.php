19fbf78635f402d1fd49fb1e2608a099f8d9291a
Jakub Malinowski <jakub-malinowski@outlook.com>
2020-12-13 17:09:31 +0100

    update add-article

--- app/views/add-article.php
+++ app/views/add-article.php
@@ -2,16 +2,28 @@
 <html lang="en">
 <?php
 require_once(_VIEWS_PATH . DIRECTORY_SEPARATOR . 'partials' . DIRECTORY_SEPARATOR . 'head.php');
+require_once(_VIEWS_PATH . DIRECTORY_SEPARATOR . 'partials' . DIRECTORY_SEPARATOR . 'header.php');
+require_once(_VIEWS_PATH . DIRECTORY_SEPARATOR . 'partials' . DIRECTORY_SEPARATOR . 'footer.php');
+require_once(_VIEWS_PATH . DIRECTORY_SEPARATOR . 'partials' . DIRECTORY_SEPARATOR . 'admin-menu.php');
+
 showHtmlHead("Dodawanie artykułu");
-include _VIEWS_PATH . DIRECTORY_SEPARATOR . 'admin-menu.php';
 ?>
+
 <body>
+    <?php
+    showHtmlHeader();
+    showHtmlAdminMenu()
+    ?>
     <form method="post" action="index.php?page=article-validation">
-        <label for="title" >Tytul</label>
+        <label for="title">Tytuł</label><br>
         <input type="text" value="" id="title" name="title" placeholder="Wpisz tytuł"><br>
-        <label for="title" >Treść</label>
+        <label for="title">Treść</label><br>
         <textarea id="content" name="content" placeholder="Wpisz treść"></textarea><br>
         <input type="submit" value="Dodaj">
     </form>
+    <?php
+    showHtmlFooter();
+    ?>
 </body>
+
 </html>
