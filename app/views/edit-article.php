284e4bd0b2c9950cd432854783f8cbb966a63b70
Jakub Malinowski <jakub-malinowski@outlook.com>
2020-12-20 00:13:25 +0100

    change style to normal

--- app/views/edit-article.php
+++ app/views/edit-article.php
@@ -13,11 +13,12 @@ showHtmlHead("Dodawanie artykułu", null, null, true);
     <main id="content">
         <?php  showHtmlAdminMenu();?>
         <h2 class="no-bcg t-center">Dodaj artykuł</h2>
-    <form class="edit-article no-bcg t-center" method="post" action="edit-article">
+    <form class="edit-article no-bcg t-center" method="post" action="edit-article" enctype="multipart/form-data">
         <label for="title">Tytuł</label><br>
         <input type="text" value="" id="title" name="title" placeholder="Wpisz tytuł" autofocus="true" required><br>
         <label for="title">Treść</label><br>
         <textarea id="content" name="content" placeholder="Wpisz treść" required rows=30></textarea><br>
+
         <input type="submit" value="Dodaj" class="button">
     </form>
     </main>
