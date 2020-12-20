22598f7c4700434c1d6302da0adfd48b1b020627
Jakub Malinowski <jakub-malinowski@outlook.com>
2020-12-20 16:44:40 +0100

    back add article

--- app/actions/edit-article.php
+++ app/actions/edit-article.php
@@ -1,20 +1,32 @@
 <?php
 require_once(_ACTIONS_PATH . 'add-picture.php');
 
-if(!isset($errors)) $errors = [];
-
 //post z form
 if(isset($_POST['title'])) {
-    validateArticle();
+    validateArticle($errors);
     $errors = validatePicture();
     $_SESSION['picture-id'] = $_POST['picture-id'];
 }
 
-function validateArticle() {
+function validateArticle($errors) {
+    $isDataCorrect = true;
+
     $articleTitle = testInput($_POST['title']);
     $articleContent = testInput($_POST['content']);
+    $photoId 
+
+    if($articleTitle == '') {
+        $isDataCorrect = false;
+        $errors['title'] = "Należy uzupełnić pole tytuł";
+    }
+
+    if()
+    
 }
 
+/**
+ * konwersja tekstu na postać bezpieczna do wstawienia do bazy sql
+ */
 function testInput($data) { 
     $data = stripslashes($data); //zabezpieczenia cudzysłowów
     $data = htmlspecialchars($data); //konwersja znaków specjalnych HTML do encji HTML
