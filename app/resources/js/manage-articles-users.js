4d3f9b3437b3361f8a8bb069c0886d7df05b59c4
Jakub Malinowski <jakub-malinowski@outlook.com>
2021-01-07 18:07:45 +0100

    hash pass; change js file name

---
+++ app/resources/js/manage-articles-users.js
@@ -0,0 +1,34 @@
+function confirmArticleDelete(homePage = "", articleId) {
+    if (confirm("Czy na pewno chcesz usunąć artykuł? Zmiany są nieodwracalne!")) {
+        
+        window.location.href = homePage + "admin-panel?delete-article=" + articleId;
+    }
+}
+
+function showHideAddingPicture() {
+    var pInput = document.getElementById('add-picture-from-file');
+    var pictureFromFileChecked = document.getElementById('picture-from-file').checked;
+
+    if (pictureFromFileChecked) {
+        pInput.style.display = 'block';
+    }
+    else {
+        pInput.style.display = 'none';
+    }
+}
+window.formSubmitted = true;//global variable
+function preventExit() {
+    window.formSubmitted = false;
+
+    window.addEventListener("beforeunload", function(e){
+        if(!formSubmitted) {
+            e.preventDefault();
+            (e || window.event).returnValue = "unload";
+            return "unload";
+        }
+    });
+    
+}
+function formSubmit() {
+    window.formSubmitted = true;
+}
